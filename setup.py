ce6a0b45e43476ac36890c80e5f4019ac8e74ffe
Jesse Korhonen <jesse.hermanni@gmail.com>
2019-02-07 11:13:54 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="morse-coder-jkorhonen",
+    version="1.0.0",
+    author="Jesse Korhonen",
+    author_email="jesse.hermanni@gmail.com",
+    description="Simple English to morse code and back converter",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jkorhonen/morse-coder",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
